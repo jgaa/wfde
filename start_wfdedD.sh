b7b4cd0fc30766c48dd9939ee80817d81a002e7a
Jarle Aase <jarle@jgaa.com>
2017-11-18 10:44:31 +0200

    Renamed

--- start_wfdedD.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start wfdedD (debug build) on port 2121 for testing/debugging
-# Logging is verbose
-
-./dbuild/tests/wfded/wfdedD -c tests/wfded/conf/simple-localhost2121.conf -L TRACE4 -C DEBUG
-
